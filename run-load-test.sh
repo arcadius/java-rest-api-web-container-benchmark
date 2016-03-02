539db2b121943ecd431c311d1bc4c773ed8cd425
Arcadius Ahouansou <arcadius.ahouansou@bskyb.com>
2016-03-02 07:16:06 +0000

    free h

--- run-load-test.sh
+++ run-load-test.sh
@@ -6,9 +6,9 @@ URL=http://localhost:8080/api
 echo " " >> result.log 2>&1
 echo "============================================================================================================ " >> result.log 2>&1
 
-echo "============`date` Warming up the API for max 720 secs or for 200 million requests, whichever comes first" >> result.log 2>&1
+echo "============`date` Warming up the API for max 720 secs or for 300 million requests, whichever comes first" >> result.log 2>&1
 echo " " >> result.log 2>&1
-ab -t 720 -n 200000000 -c 1 -d -q $URL >> result.log 2>&1
+ab -t 720 -n 300000000 -c 1 -d -q $URL >> result.log 2>&1
 echo " " >> result.log 2>&1
 echo "============`date` API warmed up. " >> result.log 2>&1
 
