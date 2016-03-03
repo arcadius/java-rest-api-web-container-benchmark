d6afb9f12793ee459e7368613c3354794f752750
Arcadius Ahouansou <arcadius.ahouansou@bskyb.com>
2016-03-03 13:33:28 +0000

    adding timeout

--- run-load-test.sh
+++ run-load-test.sh
@@ -3,15 +3,19 @@
 set -e
 #
 URL=http://localhost:8080/api
+#timeout in sec
+REQ_TIMEOUT=600
+NUM_REQUEST=100000000
+CONCURRENCY=128
 #
 echo " " >> result.log 2>&1
 echo "========================================================== " >> result.log 2>&1
 echo " " >> result.log 2>&1
 echo "====================WARM-UP======================== " >> result.log 2>&1
-echo "============`date` Warming up the API with 100 million requests" >> result.log 2>&1
+echo "============`date` Warming up the API with ${NUM_REQUEST} requests" >> result.log 2>&1
 
 echo " " >> result.log 2>&1
-ab -k -n 100000000 -c 1 -d -q $URL >> result.log 2>&1
+ab -k -n $NUM_REQUEST -c 1 -d -q -s $REQ_TIMEOUT $URL >> result.log 2>&1
 echo " " >> result.log 2>&1
 
 
@@ -24,7 +28,7 @@ echo "====================BENCHMARK======================== " >> result.log 2>&1
 
 echo "============`date` Running load test ..." >> result.log 2>&1
 #The benchmark
-ab -k -n 100000000 -c 128 $URL >> result.log 2>&1
+ab -k -n $NUM_REQUEST -c $CONCURRENCY -s $REQ_TIMEOUT $URL >> result.log 2>&1
 echo " " >> result.log 2>&1
 echo "============`date` Done ..." >> result.log 2>&1
 echo " " >> result.log 2>&1
